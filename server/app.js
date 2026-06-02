2b7d5c42e72227cba7001a00fcd96a2615f13144
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-06-02 09:32:59 +0700

    tạo file

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import movieRoutes from "./src/routes/movieRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Cinema backend is running...");
 });
+
+app.use("/api/movies", movieRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/CINEMA";
