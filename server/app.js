77c2b7dcde4f26a8dd601f03a12fbeca85563356
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-06-02 09:24:11 +0700

    chỉnh lại BE sang JS

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Cinema backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/CINEMA";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Ket noi CSDL thanh cong");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Ket noi CSDL that bai:", error.message);
+  });
