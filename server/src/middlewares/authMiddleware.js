3549c86b421c232d431674cab912553cfa6f3250
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-06-05 11:54:04 +0700

    tạo signup signin và gitignore

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,1 +1,18 @@
 //
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { asyncHandler } from "../utils/asynHandler.js";
+export const protect = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.token
+        || req.headers.authorization?.replace("Bearer ", "");
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Chưa đăng nhập" });
+    }
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(decoded.id);
+    if (!user || !user.isActive) {
+        return res.status(401).json({ success: false, message: "Token không hợp lệ hoặc tài khoản bị khoá" });
+    }
+    req.user = user;
+    next();
+});
