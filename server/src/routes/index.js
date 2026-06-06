805472be8e19fc0b842c814ca55e0c93c45022ca
luandevweb2302 <goblin3st@gmail.com>
2026-06-06 22:18:49 +0700

    add controllers crud categorycategory

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,14 @@
 import express from "express"
 import routerMovie from "./movieRoutes.js";
 import routerAuth from "./authRoutes.js";
+//// Routers category
+import categoryRoutes from "./categoryRoutes.js";
+
 const router = express.Router();
 
 router.use("/movies", routerMovie);
-router.use("/auth", routerAuth)
+router.use("/auth", routerAuth);
+// Routers category
+router.use("/categories", categoryRoutes);
+
 export default router
