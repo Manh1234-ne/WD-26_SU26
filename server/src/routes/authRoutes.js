731b7d6d58ab2123da2e518db326828b63a02eb2
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-06-10 16:32:58 +0700

    tạo google SignIn và changsePassword

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { signIN, signUP, signOut } from "../controllers/authController.js";
+import { signIN, signUP, signOut, googleSignIn, changePassword } from "../controllers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const routerAuth = express.Router();
 
 routerAuth.post("/signUp", signUP);
 routerAuth.post("/signIn", signIN);
 routerAuth.post("/signOut", signOut);
-
+routerAuth.post("/google_signIn", googleSignIn)
+routerAuth.post("/change_password", protect, changePassword);
 export default routerAuth
