072cbec02a85394308bf9b0ef22d15a8873d50fc
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-07-10 23:44:51 +0700

    thêm trailer vào detailmovie

--- client/src/pages/client/MovieDetail.tsx
+++ client/src/pages/client/MovieDetail.tsx
@@ -24,7 +24,18 @@ function MovieDetail() {
       .catch(() => setError('không thể tải trang chi tiết phim...'))
       .finally(() => setIsLoading(false))
   }, [id])
+  const getEmbedUrl = (url: string) => {
+    if (url.includes("watch?v=")) {
+      return url.replace("watch?v=", "embed/");
+    }
 
+    if (url.includes("youtu.be/")) {
+      const id = url.split("youtu.be/")[1];
+      return `https://www.youtube.com/embed/${id}`;
+    }
+
+    return url;
+  };
   if (isLoading) {
     return <Loading fullScreen text="Đang tải thông tin phim..." />
   }
@@ -41,47 +52,67 @@ function MovieDetail() {
   }
 
   return (
-    <section className="detail-page">
-      <div className="detail-poster">
-        {movie.posterUrl ? (
-          <img src={movie.posterUrl} alt={movie.title} />
-        ) : (
-          <div className="poster-fallback">{movie.title.charAt(0)}</div>
-        )}
-      </div>
-      <div className="detail-copy">
-        <p className="eyebrow">{movie.genres?.join(' / ') || 'LUMORA'}</p>
-        <h1>{movie.title}</h1>
-        {movie.originalTitle && <h2>{movie.originalTitle}</h2>}
-        <p>{movie.description}</p>
-        <dl className="detail-list">
-          <div>
-            <dt>Thời lượng</dt>
-            <dd>{movie.duration} phút</dd>
-          </div>
-          <div>
-            <dt>Ngôn Ngữ</dt>
-            <dd>{movie.language || "đang cập nhật"}</dd>
-          </div>
-          <div>
-            <dt>Đạo diễn</dt>
-            <dd>{movie.director || 'đang cập nhật'}</dd>
-          </div>
-          <div>
-            <dt>Độ tuổi</dt>
-            <dd>{movie.ageRating}</dd>
-          </div>
-        </dl>
-        <div className="hero-actions">
-          <Link className="primary-button" to={`/movies/${movie._id}/showtimes`}>
-            Đặt vé ngay
-          </Link>
-          <Link className="ghost-button" to="/">
-            Quay lại
-          </Link>
+    <div className="detail-page-container">
+      <section className="detail-page">
+        <div className="detail-poster">
+          {movie.posterUrl ? (
+            <img src={movie.posterUrl} alt={movie.title} />
+          ) : (
+            <div className="poster-fallback">{movie.title.charAt(0)}</div>
+          )}
         </div>
-      </div>
-    </section>
+        <div className="detail-copy">
+          <p className="eyebrow">{movie.genres?.join(' / ') || 'LUMORA'}</p>
+          <h1>{movie.title}</h1>
+          {movie.originalTitle && <h2>{movie.originalTitle}</h2>}
+          <p>{movie.description}</p>
+          <dl className="detail-list">
+            <div>
+              <dt>Thời lượng</dt>
+              <dd>{movie.duration} phút</dd>
+            </div>
+            <div>
+              <dt>Ngôn Ngữ</dt>
+              <dd>{movie.language || "đang cập nhật"}</dd>
+            </div>
+            <div>
+              <dt>Đạo diễn</dt>
+              <dd>{movie.director || 'đang cập nhật'}</dd>
+            </div>
+            <div>
+              <dt>Độ tuổi</dt>
+              <dd>{movie.ageRating}</dd>
+            </div>
+          </dl>
+          <div className="hero-actions">
+            <Link className="primary-button" to={`/movies/${movie._id}/showtimes`}>
+              Đặt vé ngay
+            </Link>
+            {movie.trailerUrl && (
+              <a href="#trailer" className="ghost-button">
+                Xem Trailer
+              </a>
+            )}
+            <Link className="ghost-button" to="/">
+              Quay lại
+            </Link>
+          </div>
+        </div>
+      </section>
+      {movie.trailerUrl && (
+        <section id="trailer" className='detail-trailer-section'>
+          <h2 className="section-title">Trailer phim</h2>
+          <div className='ifram-container'>
+            <iframe
+              src={getEmbedUrl(movie.trailerUrl)}
+              title={`${movie.title}`}
+              allowFullScreen
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            ></iframe>
+          </div>
+        </section>
+      )}
+    </div>
   )
 }
 
