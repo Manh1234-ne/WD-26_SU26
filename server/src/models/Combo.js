e56f144d747a2a6533bf04750f38b415163368f5
lebang271206 <lebang271206@gmail.com>
2026-07-19 17:48:46 +0700

    model combo

---
+++ server/src/models/Combo.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const comboSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Combo", comboSchema);
