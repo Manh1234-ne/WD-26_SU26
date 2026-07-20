b18dd3460a6edac131316d91873f044446fdfcd1
lebang271206 <lebang271206@gmail.com>
2026-07-20 22:45:51 +0700

    xử lí API

---
+++ server/src/controllers/inventoryController.js
@@ -0,0 +1,29 @@
+import InventoryItem from "../models/InventoryItem.js";
+import { asyncHandler } from "../utils/asynHandler.js";
+
+const ok = (res, data) =>
+  res.status(200).json({
+    success: true,
+    data,
+  });
+
+const created = (
+  res,
+  data,
+  message = "Tạo thành công"
+) =>
+  res.status(201).json({
+    success: true,
+    message,
+    data,
+  });
+
+const fail = (
+  res,
+  status,
+  message
+) =>
+  res.status(status).json({
+    success: false,
+    message,
+  });
